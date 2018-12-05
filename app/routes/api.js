fc927705e8697a4d6c714edd3d3ff90f7de11f5f
Ali <asammour33@gmail.com>
2018-12-05 04:29:29 -0500

    Error Message Fixes

--- app/routes/api.js
+++ app/routes/api.js
@@ -7,17 +7,17 @@ module.exports = function(router){
         user.email = req.body.email;
         user.password =  req.body.password;
         if(req.body.email == null || req.body.email == '' || req.body.password == null || req.body.password == ''){
-            return res.status(400).send('Please provide an email and password');
+            return res.status(400).send('Please provide an email and password: ');
         } else {
             user.save(function(err){
                 if (err){
-                    return res.status(400).send('Email is already being used');
+                    return res.status(400).send('That email is already being used!');
                 } else {
                     Helpers.sendVerifcationEmail(user, function(eror){
                         if(eror){
                             return res.status(400).send('Error while creating account. Please try again later!');
                         } else {
-                            return res.status(200).send('Account Created and Verification email has been sent. Please check your inbox and verify email!');          
+                            return res.status(200).send('Account Created and Verification email has been sent. Please check your inbox and verify your email!');          
                         }
                     })
                 }
@@ -38,7 +38,7 @@ module.exports = function(router){
                     res.json({sucess: false, message: 'No password'});
                 }
                 if (!validPassword){
-                    res.json({sucess: false, message: 'Could not authenticate password'});
+                    res.json({sucess: false, message: 'Could not authenticate the password'});
                 } else {
                     res.json({sucess: true, message: 'Authenticated!', email: user.email});
                 }
